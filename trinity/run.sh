e24694e9bd13692a145a4bffe9de64883f16ce6e
Mike Burr <mburr@unintuitive.org>
2021-05-06 17:56:37 -0400

    better daemon

--- trinity/run.sh
+++ trinity/run.sh
@@ -1,1 +1,5 @@
-trinity --genesis ~/.local/share/trinity/girly.json --trinity-root-dir ~/.local/share/trinity/girly --data-dir ~/.local/share/trinity/girly/chain-eth1 --network-id 4711 --disable-tx-pool
+#!/bin/sh -e
+
+export PYTHONWARNINGS="ignore::DeprecationWarning"
+
+trinity --genesis ~/git/sandbox/trinity/girly.json --trinity-root-dir ~/.local/share/trinity/girly --data-dir ~/.local/share/trinity/girly/chain-eth1 --network-id 4711 --disable-tx-pool --log-level 20
