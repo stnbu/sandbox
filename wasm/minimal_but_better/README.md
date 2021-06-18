6b48e107d5ffa08e35a35dffddc2991d1accafd1
Mike Burr <mburr@unintuitive.org>
2021-06-18 13:20:12 -0400

    .

--- wasm/minimal_but_better/README.md
+++ wasm/minimal_but_better/README.md
@@ -1,6 +1,12 @@
-Maybe `--reference-types`?
+Maybe `--reference-types`? (only Firefox 79+ and Chrome)
 
 ```
 cargo build --target wasm32-unknown-unknown
-wasm-bindgen --out-dir pkg --target web --no-typescript --omit-default-module-path target/wasm32-unknown-unknown/debug/minimal_but_better.wasm
+wasm-bindgen --out-dir pkg --target web --reference-types --no-typescript --omit-default-module-path target/wasm32-unknown-unknown/debug/minimal_but_better.wasm
+```
+
+Hmm. This happens in Chrome `91.0.4472.106 (Official Build) (x86_64)` but works in Firefox `89.0.1 (64-bit)`...
+
+```
+Uncaught (in promise) CompileError: WebAssembly.instantiateStreaming(): invalid value type 'externref', enable with --experimental-wasm-reftypes @+25
 ```
--- wasm/minimal_but_better/src/lib.rs
+++ wasm/minimal_but_better/src/lib.rs
@@ -1,32 +1,24 @@
 use wasm_bindgen::prelude::*;
-use std::collections::HashMap;
-use serde::{Serialize, Deserialize};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn it_works() {
-	let json = send_example_to_js();
-	assert!(!json.is_string())
-    }
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct Example {
-    pub field1: HashMap<u32, String>,
-    pub field2: Vec<Vec<f32>>,
-    pub field3: [f32; 4],
+#[wasm_bindgen(start)]
+pub fn start() -> Result<(), JsValue> {
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+    let val = document.create_element("p")?;
+    val.set_inner_html("Hello from Rust!");
+    body.append_child(&val)?;
+    let me = "Me!";
+    console_log!("Hello, from {}", me);
+    Ok(())
 }
 
 #[wasm_bindgen]
-pub fn send_example_to_js() -> JsValue {
-    let mut field1 = HashMap::new();
-    field1.insert(0, String::from("ex"));
-    let example = Example {
-        field1,
-        field2: vec![vec![1., 2.], vec![3., 4.]],
-        field3: [1., 2., 3., 4.]
-    };
-    JsValue::from_serde(&example).unwrap()
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
