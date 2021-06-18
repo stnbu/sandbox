a2f076252368bbdebd9a433eadac19646a4dc307
Mike Burr <mburr@unintuitive.org>
2021-06-18 14:31:34 -0400

    play with "inspectable"

--- wasm/serde_play/src/lib.rs
+++ wasm/serde_play/src/lib.rs
@@ -12,6 +12,37 @@ mod tests {
     }
 }
 
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+#[wasm_bindgen(start)]
+pub fn start() -> Result<(), JsValue> {
+    let x = send_example_to_js();
+    console_log!("Example: {:#?}", x);
+    Ok(())
+}
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen(inspectable)]
+pub struct Baz {
+    pub field: i32,
+    private: i32,
+}
+
+#[wasm_bindgen]
+impl Baz {
+    #[wasm_bindgen(constructor)]
+    pub fn new(field: i32) -> Baz {
+        Baz { field, private: 13 }
+    }
+}
+
 #[derive(Serialize, Deserialize)]
 pub struct Example {
     pub field1: HashMap<u32, String>,
