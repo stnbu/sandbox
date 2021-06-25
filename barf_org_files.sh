8513411f0cc0a86f84c72d3eacd239bd4c8b1b4f
Mike Burr <mburr@unintuitive.org>
2021-06-25 16:56:33 -0400

    .

--- barf_org_files.sh
+++ barf_org_files.sh
@@ -2,7 +2,15 @@
 
 # ./this_script.sh <org_file_name> <dapps_url_relative_path>
 
-emacs "$1" --batch -f org-html-export-to-html --kill >/dev/null 2>&1
-OUTPUT_FILE_NAME=$(echo "$1" | sed 's/\(.*\.\)org$/\1html/')
-# we are depending on -e here
-mv "$OUTPUT_FILE_NAME" /var/www/dapps.unintuitive.org/"$2"
+if [ "${1-}" = "" ] ; then
+    SRC=/home/mburr/git/sandbox/all.org
+    DST=/var/www/dapps.unintuitive.org/index.html
+else
+    SRC=$1
+    DST=$1
+fi
+
+emacs "$SRC" --batch -f org-html-export-to-html --kill >/dev/null 2>&1
+OUTPUT_FILE_NAME=$(echo "$SRC" | sed 's/\(.*\.\)org$/\1html/')
+mv "$OUTPUT_FILE_NAME" "$DST"
+echo -n "Happy!"
