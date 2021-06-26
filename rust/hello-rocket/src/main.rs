ce1862a9708a07042c80b43658a2fd98f5d3d917
Mike Burr <mb@unintuitive.org>
2021-06-26 11:57:26 -0600

    .

--- rust/hello-rocket/src/main.rs
+++ rust/hello-rocket/src/main.rs
@@ -1,11 +1,27 @@
 use rocket::tokio::time::{sleep, Duration};
 use std::path::{Path, PathBuf};
 use rocket::fs::NamedFile;
+use rocket::http::Status;
+use rocket::response::{content, status};
+use std::sync::atomic::Ordering;
+use std::sync::atomic::AtomicUsize;
+use rocket::State;
 
 #[rocket::get("/")]
 fn hello() -> &'static str {
     "Hello, world!"
 }
+
+#[rocket::get("/teapot")]
+fn teapot() -> status::Custom<content::Json<&'static str>> {
+    status::Custom(Status::ImATeapot, content::Json("{ \"hi\": \"world\" }"))
+}
+
+/////// TODO: The plan here was to print the number of segmens. Do you know how to do that _now_?
+// #[rocket::get("segments/<segments..>/end")]
+// fn hello(segments: &[str]) -> &'static str {
+//     ""
+// }
 
 #[rocket::get("/delay/<seconds>")]
 async fn delay(seconds: u64) -> String {
@@ -35,10 +51,22 @@ async fn files(file: PathBuf) -> Option<NamedFile> {
 //     rocket::build().mount("/", rocket::routes![hello])
 // }
 
+struct HitCount {
+    count: AtomicUsize
+}
+
+#[rocket::get("/count")]
+fn count(hit_count: &State<HitCount>) -> String {
+    let current_count = hit_count.count.load(Ordering::Relaxed);
+    // Ok, but this doesn't increment
+    format!("Number of visits: {}", current_count)
+}
+
 #[rocket::main]
 async fn main() {
     let _ = rocket::build()
-        .mount("/", rocket::routes![delay, arbitrary, files])
+        .mount("/", rocket::routes![delay, arbitrary, files, teapot, count, hello])
+	.manage(HitCount { count: AtomicUsize::new(0) })
         .launch()
         .await;
 }
