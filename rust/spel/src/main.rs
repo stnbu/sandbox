dc3664dba3b5b4651645412a76f8d6b5113b1d68
Mike Burr <mb@unintuitive.org>
2021-06-27 10:29:08 -0600

    .

--- rust/spel/src/main.rs
+++ rust/spel/src/main.rs
@@ -7,15 +7,25 @@ use std::sync::atomic::Ordering;
 use std::sync::atomic::AtomicUsize;
 use rocket::State;
 
+//// For now, we support one word. We would like, e.g.
+////   /word1/word2/... (silly but looks nice)
+////   /?word2&word2&... (probably no less silly, actually)
 #[rocket::get("/<word>")]
-fn hello(word: &str) -> String {
-    format!("You cannot spell _{}_ ?!", String::from(word).to_uppercase()) // does not _feel_ right
+fn spell(word: &str) -> content::Json<&'static str> {
+    println!("[INFO] Hey, this guy cannot spell \"{}\". LOL!", word);
+    content::Json("{ \"suggestion\": \"<buy a dictionary?>\" }")
+}
+
+fn get_sugggestions(word: &str) -> Vec<&str> {
+    let ten_millis = std::time::Duration::from_millis(10);
+    std::thread::sleep(ten_millis);
+    vec!["maybe this", "or maybe this"]
 }
 
 #[rocket::main]
 async fn main() {
     let _ = rocket::build()
-        .mount("/", rocket::routes![hello])
+        .mount("/", rocket::routes![spell])
         .launch()
         .await;
 }
