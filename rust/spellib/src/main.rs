83af33823fee699704fea754de3c64d4237f2b66
Mike Burr <mb@unintuitive.org>
2021-06-28 15:34:27 -0600

    store in lowercase, trysee distance==2

--- rust/spellib/src/main.rs
+++ rust/spellib/src/main.rs
@@ -12,11 +12,11 @@ fn main() {
     println!("Composing BKTree...");
     for (index, line) in lines.enumerate() {
 	println!("{}", 235886 - index);
-	tree.add(line.unwrap());
+	tree.add(line.unwrap().to_lowercase());
     }
     let size = std::mem::size_of_val(&tree);
     println!("\nDone! ({} bytes)", size);
 
-    let benchlike = tree.find("bench", 1).collect::<Vec<_>>();
-    println!("distance-1 from bench: {:?}", benchlike)
+    let benchlike = tree.find("bench", 2).collect::<Vec<_>>();
+    println!("distance-2 from bench: {:?}", benchlike)
 }
