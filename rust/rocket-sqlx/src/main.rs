d34228c57e8ccc0da6fd8c1cbc9dd46f6f042190
Mike Burr <mb@unintuitive.org>
2021-06-28 11:27:28 -0600

    ripoff of Rocket/examples/databases ... minimized to only do sqlite

---
+++ rust/rocket-sqlx/src/main.rs
@@ -0,0 +1,12 @@
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_sync_db_pools;
+
+#[cfg(test)] mod tests;
+
+mod sqlx;
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .attach(sqlx::stage())
+}
