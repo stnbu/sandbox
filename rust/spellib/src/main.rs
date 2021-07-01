68602414d55de7f5177e1d24955745d97febb2ec
Mike Burr <mb@unintuitive.org>
2021-07-01 13:11:14 -0600

    serde_cbor experimentation

--- rust/spellib/src/main.rs
+++ rust/spellib/src/main.rs
@@ -1,8 +1,16 @@
 use bk_tree::{BKTree, metrics};
 
+use serde_derive::{Deserialize, Serialize};
+use std::error::Error;
+
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct StringBKTree {
+    tree: BKTree<String>
+}
 
 fn main() {
     let mut tree: BKTree<String> = BKTree::new(metrics::Levenshtein);
@@ -20,3 +28,23 @@ fn main() {
     let benchlike = tree.find("bench", 2).collect::<Vec<_>>();
     println!("distance-2 from bench: {:?}", benchlike)
 }
+
+
+
+// fn main2() -> Result<(), Box<dyn Error>> {
+//     let ferris = Mascot {
+//         name: "Ferris".to_owned(),
+//         species: "crab".to_owned(),
+//         year_of_birth: 2015,
+//     };
+
+//     let ferris_file = File::create("words.cbor")?;
+//     serde_cbor::to_writer(ferris_file, &ferris)?;
+
+//     let tux_file = File::open("words.cbor")?;
+//     let tux: Mascot = serde_cbor::from_reader(tux_file)?;
+
+//     println!("{:?}", tux);
+
+//     Ok(())
+// }
