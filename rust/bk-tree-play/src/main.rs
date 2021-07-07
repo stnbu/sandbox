d500c3cd7ca734574a783bba83caaacd87710026
Mike Burr <mb@unintuitive.org>
2021-07-07 14:54:18 -0600

    .

--- rust/bk-tree-play/src/main.rs
+++ rust/bk-tree-play/src/main.rs
@@ -1,4 +1,4 @@
-use bk_tree::BKTree;
+use bk_tree::{BKNode, BKTree};
 //use bk_tree::serde_cbor;
 
 fn main() {
@@ -6,8 +6,8 @@ fn main() {
     tree.add("book".to_string());
     //tree.add("book");
     println!("Bodes: {:?}", tree.find("", 4).collect::<Vec<_>>());
-    let bytes = &tree.to_vec().unwrap();
+    let bytes = &tree.root.unwrap().to_vec().unwrap();
     //println!("Serialized: {:?}", &bytes);
-    let tree: Result<BKTree<String>, serde_cbor::error::Error> = BKTree::from_slice(&bytes.as_ref().unwrap()[..], bk_tree::metrics::Levenshtein);
-    println!("Tree: {:?}", tree);
+    let node: BKNode<String> = BKNode::from_slice(&bytes).unwrap();
+    println!("Tree: {:?}", node);
 }
