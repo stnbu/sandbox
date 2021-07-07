9eef60bcea73cc9590eca60c6411250f82c07626
Mike Burr <mb@unintuitive.org>
2021-07-07 07:42:31 -0600

    works with bk_tree@5e8d05e91ffdb38a073582c90628e2c9dd590c9a .. notice the `&str`->`String` .. `&str` worked fine when calling `serde_cbor::{to_vec, from_slice}` directly. The lifetime business related to `serde::Deserialize<'de>` is hard to understand.

--- rust/bk-tree-play/src/main.rs
+++ rust/bk-tree-play/src/main.rs
@@ -2,12 +2,12 @@ use bk_tree::BKTree;
 //use bk_tree::serde_cbor;
 
 fn main() {
-    let mut tree: BKTree<&str> = Default::default();
-    tree.add("book");
-    tree.add("book");
+    let mut tree: BKTree<String> = Default::default();
+    tree.add("book".to_string());
+    //tree.add("book");
     println!("Bodes: {:?}", tree.find("", 4).collect::<Vec<_>>());
     let bytes = &tree.to_vec().unwrap();
-    println!("Serialized: {:?}", &bytes);
-    // let tree = BKTree::<&str>::from_slice(&bytes, bk_tree::metrics::Levenshtein);
-    // println!("Tree: {:?}", tree);
+    //println!("Serialized: {:?}", &bytes);
+    let tree: Result<BKTree<String>, serde_cbor::error::Error> = BKTree::from_slice(&bytes.as_ref().unwrap()[..], bk_tree::metrics::Levenshtein);
+    println!("Tree: {:?}", tree);
 }
