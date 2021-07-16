27b7d93fbe111066a5e25345f572d5b9799afb9b
Mike Burr <mb@unintuitive.org>
2021-07-16 14:43:17 -0600

    but this should work tho

--- rust/flargs_play/src/main.rs
+++ rust/flargs_play/src/main.rs
@@ -2,13 +2,13 @@
 /// * One way to generalzie returned errors.
 
 #[derive(Debug)]
-struct VaryInt {
-    value: u8,
+enum VaryInt {
+    Some(u8)
 }
 
 impl From<u8> for VaryInt {
     fn from(value: u8) -> Self {
-        Self { value }
+        Self::Some(value)
     }
 }
 
@@ -16,19 +16,20 @@ impl From<Option<u8>> for VaryInt {
     fn from(o: Option<u8>) -> Self {
 	match o {
 	    Some(value) => {
-		VaryInt { value }
+		VaryInt::Some(value)
 	    },
 	    None => {
-		VaryInt { value: 0 }
+		VaryInt::Some(0)
 	    },
 	}
     }
 }
 
-// fn test(x: VaryInt) {
-//     println!("--> {:?}", x);
-// }
+fn test(x: VaryInt) {
+    println!("--> {:?}", x);
+}
 
 fn main() {
-    //test(None);
+    test(None);
+    test(3);
 }
