e0d6b7d4e4c1f5dcdb7748efe77a38af2605c623
Mike Burr <mb@unintuitive.org>
2021-07-16 14:34:57 -0600

    compiles

--- rust/flargs_play/src/main.rs
+++ rust/flargs_play/src/main.rs
@@ -1,44 +1,34 @@
 /// * Practical use of the From trait.
 /// * One way to generalzie returned errors.
 
-use std::fs;
-use std::io;
-use std::num;
-
 #[derive(Debug)]
-enum CliError {
-    IoError(io::Error),
-    ParseError(num::ParseIntError),
+struct VaryInt {
+    value: u8,
 }
 
-impl From<io::Error> for CliError {
-    fn from(error: io::Error) -> Self {
-        CliError::IoError(error)
+impl From<u8> for VaryInt {
+    fn from(value: u8) -> Self {
+        Self { value }
     }
 }
 
-impl From<num::ParseIntError> for CliError {
-    fn from(error: num::ParseIntError) -> Self {
-        CliError::ParseError(error)
+impl From<Option<u8>> for VaryInt {
+    fn from(o: Option<u8>) -> Self {
+	match o {
+	    Some(value) => {
+		VaryInt { value }
+	    },
+	    None => {
+		VaryInt { value: 0 }
+	    },
+	}
     }
 }
 
-fn open_and_parse_file(file_name: &str) -> Result<i32, CliError> {
-    let mut contents = fs::read_to_string(&file_name)?;
-    let num: i32 = contents.trim().parse()?;
-    Ok(num)
-}
-
+// fn test(x: VaryInt) {
+//     println!("--> {:?}", x);
+// }
 
 fn main() {
-    for path in &["/dev/null", "/nowhere", "/tmp/this_file_contains_3"] {
-	match open_and_parse_file(&path) {
-	    Ok(n) => {
-		println!(" File {} had {}", &path, n);
-	    },
-	    Err(e) => {
-		println!(" File {} err {:?}", &path, e);
-	    },
-	};
-    }
+    //test(None);
 }
