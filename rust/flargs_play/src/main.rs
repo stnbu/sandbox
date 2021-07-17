a98140b4f4d1c4daeb5679db466fbcdf3c0eb34d
Mike Burr <mb@unintuitive.org>
2021-07-17 08:27:25 -0600

    neat!

--- rust/flargs_play/src/main.rs
+++ rust/flargs_play/src/main.rs
@@ -1,10 +1,12 @@
-/// * Practical use of the Into trait.
-/// * One way to generalzie returned errors.
+/// * This is usful and took some time to grok
+/// * It introduces `impl` in a context that might be new and was to me.
+/// * It allows you to specify one of two types in function artuments.
+/// * Could it be extended for your own enum `Some(u32) || Some(&str) || Some(Vec<u8>) || None` ...?
 
 // thank you, sir: https://discord.com/channels/273534239310479360/273541522815713281/865704536287084555
-fn test(x: impl Into<Option<u8>>) {
+fn test(x: impl Into<Option<u32>>) {
     match x.into() {
-        Some(n) => println!("got {}", n),
+        Some(n) => println!("got {:?}", n),
         None => println!("got None"),
     }
 }
