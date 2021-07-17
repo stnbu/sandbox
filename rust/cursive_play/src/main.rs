0ec7d11ea5e335c53e0cc8cb002c1ee44c389941
Mike Burr <mb@unintuitive.org>
2021-07-17 09:18:08 -0600

    I am in love.

--- rust/cursive_play/src/main.rs
+++ rust/cursive_play/src/main.rs
@@ -1,3 +1,9 @@
+use cursive::{Cursive, CursiveExt};
+use cursive::views::TextView;
+
 fn main() {
-    println!("Hello, world!");
+    let mut siv = Cursive::new();
+    siv.add_layer(TextView::new("Hello World!\nPress q to quit."));
+    siv.add_global_callback('q', |s| s.quit());
+    siv.run();
 }
