717fb7e1ca290d79b04119de804eebbca36b0c9d
Mike Burr <mb@unintuitive.org>
2021-07-23 12:20:55 -0600

    pretty much done

--- rust/dsfs/src/main.rs
+++ rust/dsfs/src/main.rs
@@ -1,8 +1,26 @@
+/*
+Best-in-class CLI web server:
 
+```
+dsfs ./   # serves current directory at localhost:8000
+dsfs      # serves current directory at localhost:8000
+dsfs /tmp # serves /tmp/ localhost:8000
+```
+ */
+
+use std::env;
 use rocket_contrib::serve::StaticFiles;
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let path = match args.len() {
+	1 => "./",
+	2 => &args[1],
+	_ => panic!("Usage: {} [path]\n\tServe content of `path` directiory at localhost:8000",
+		    args[0]),
+    };
     rocket::ignite()
-        .mount("/public", StaticFiles::from("/static"))
+        .mount("/", StaticFiles::from(path))
         .launch();
 }
 
