14fb53bdbc9afb92bd7376cfe0536dba6f5dd314
Mike Burr <mb@unintuitive.org>
2021-07-27 08:15:52 -0600

    .

--- rust/spelib/src/lib.rs
+++ rust/spelib/src/lib.rs
@@ -20,9 +20,9 @@ words. Suggest "flower", get "disestablishmentarianism" as a result...
 //!
 //! Word distance is calculated using the Levenshtein metric.
 
-use bk_tree::{BKTree, BKNode, metrics};
-use std::io::prelude::*;
+use bk_tree::{metrics, BKNode, BKTree};
 use std::fs::File;
+use std::io::prelude::*;
 use std::io::{self, BufRead};
 use std::path::Path;
 //use anyhow::Result;
@@ -34,71 +34,74 @@ static SEARCH_DISTANCE: u32 = 1;
 pub struct SearchTree {
     tree: BKTree<String>,
 }
-use std::error::Error;
+//use std::error::Error;
 
 impl SearchTree {
     /// Pass your query `word` and get a `Vec<String>` of words that are `odistance` or nearer your word.
     pub fn search<'a>(&self, word: &str, odistance: Option<u32>) -> Result<Vec<String>, &'a str> {
-	let distance: u32 = match odistance {
-	    None => SEARCH_DISTANCE,
-	    Some(d) => d,
-	};
-	let mut results: Vec<String> = Vec::new();
-	for (_, result) in self.tree.find(word, distance) {
-	    results.push(result.to_string());
-	}
-	Ok(results)
+        let distance: u32 = match odistance {
+            None => SEARCH_DISTANCE,
+            Some(d) => d,
+        };
+        let mut results: Vec<String> = Vec::new();
+        for (_, result) in self.tree.find(word, distance) {
+            results.push(result.to_string());
+        }
+        Ok(results)
     }
     pub fn new_from_words_path() -> Result<Self, std::io::Error> {
-	let mut tree: BKTree<String> = BKTree::new(metrics::Levenshtein);
-	let file = File::open(SYSTEM_WORD_FILE)?;
-	let lines = io::BufReader::new(file).lines();
-	for line in lines {
-	    tree.add(line.unwrap().to_lowercase());
-	};
-	Ok(Self{tree})
+        let mut tree: BKTree<String> = BKTree::new(metrics::Levenshtein);
+        let file = File::open(SYSTEM_WORD_FILE)?;
+        let lines = io::BufReader::new(file).lines();
+        for line in lines {
+            tree.add(line.unwrap().to_lowercase());
+        }
+        Ok(Self { tree })
     }
     pub fn new_from_serialized_path(path: &Path) -> anyhow::Result<Self> {
-	let file = File::open(&path)?;
-	let mut bytes: Vec<u8> = Vec::new();
-	let _ = file.read_to_end(&mut bytes);
-	Self::new_from_bytes(&bytes)
+        let file = File::open(&path)?;
+        let mut bytes: Vec<u8> = Vec::new();
+        let _ = file.read_to_end(&mut bytes);
+        Self::new_from_bytes(&bytes)
     }
     pub fn new_from_bytes(bytes: &[u8]) -> Result<Self, serde_cbor::error::Error> {
-	match BKNode::from_slice(bytes) {
-	    Ok(node) => {
-		let mut tree: BKTree<String> = BKTree::new(metrics::Levenshtein);
-		tree.root = Some(node);
-		Ok(Self{tree})
-	    },
-	    Err(e) => Err(e),
-	}
+        match BKNode::from_slice(bytes) {
+            Ok(node) => {
+                let mut tree: BKTree<String> = BKTree::new(metrics::Levenshtein);
+                tree.root = Some(node);
+                Ok(Self { tree })
+            }
+            Err(e) => Err(e),
+        }
     }
     pub fn write_to_path(&self, path: &Path) -> Result<u32, std::io::Error> {
-	let bytes = &self.tree.root.unwrap().to_vec()?;
-	let mut file = match File::create(&path) {
+        let bytes = &self.tree.root.unwrap().to_vec()?;
+        let mut file = match File::create(&path) {
             Err(e) => panic!("Create Err: {}", e),
             Ok(file) => file,
-	};
-	file.write_all(&bytes)?
+        };
+        file.write_all(&bytes)?
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::{SearchTree, BKTREE_STORE_FILE};
     use std::path::Path;
-    use crate::{
-	SearchTree,
-	BKTREE_STORE_FILE,
-    };
 
     #[test]
     fn test_all() {
-	//use std::fs;
-	//fs::remove_file(BKTREE_STORE_FILE).ok();
-	let path = Path::new(BKTREE_STORE_FILE);
-	let tree = SearchTree::new_from_serialized_path(&path);
-	let results: Vec<String> = tree.search("robert", None).unwrap().into_iter().collect();
-	assert!(results.len() == 5)
+        //use std::fs;
+        //fs::remove_file(BKTREE_STORE_FILE).ok();
+        let path = Path::new(BKTREE_STORE_FILE);
+        let foo = path;
+        let tree = SearchTree::new_from_serialized_path(&path);
+        let results: Vec<String> = tree
+            .unwrap()
+            .search("robert", None)
+            .unwrap()
+            .into_iter()
+            .collect();
+        assert!(results.len() == 5)
     }
 }
