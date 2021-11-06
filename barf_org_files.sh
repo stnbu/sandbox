2fe9312d6b303cf911417bbb63b5eb1cbf0590ab
Mike Burr <mburr@unintuitive.org>
2021-11-06 07:26:00 -0600

    .

--- barf_org_files.sh
+++ barf_org_files.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -ue
+#!/bin/sh -xue
 
 # ./this_script.sh <org_file_name> <dapps_url_relative_path>
 
@@ -10,7 +10,7 @@ else
     DST=$1
 fi
 
-emacs "$SRC" --batch -f org-html-export-to-html --kill >/dev/null 2>&1
+/usr/local/bin/emacs "$SRC" --batch -f org-html-export-to-html --kill >/dev/null 2>&1
 OUTPUT_FILE_NAME=$(echo "$SRC" | sed 's/\(.*\.\)org$/\1html/')
 rsync -xa "$OUTPUT_FILE_NAME" pu:"$DST"
 echo -n "Happy!"
