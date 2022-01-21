f5dac1a3ed6536ab45bdb35128d89e4310f9a0a0
Mike Burr <mb@unintuitive.org>
2022-01-21 13:33:04 -0700

    .

--- manim_play/zip_zap_rap.py
+++ manim_play/zip_zap_rap.py
@@ -5,7 +5,7 @@ from decimal import Decimal
 class W(Scene):
     def construct(self):
         modulus = 10
-        axis_config = {"scaling": WrappedScale(Decimal(modulus))}
+        axis_config = {}  #{"scaling": WrappedScale(Decimal(modulus))}
         t_range = [-10, 10, 1]
         ax = Axes(
             x_range=t_range,
@@ -15,16 +15,12 @@ class W(Scene):
             x_axis_config=axis_config,
             y_axis_config=axis_config
         ).add_coordinates()
-        #graph = ax.plot(lambda x: x ** 2, x_range=[0.001, 30, 0.5], use_smoothing=False)
-        self.add(ax)
-        m = 100
-        for n in range(int(0.01*m), int(30*m), int(0.2*m)):
-            x = n / 100
-            y = x ** 2
-            dot = Circle(radius=0.01, color=RED, fill_opacity=1).move_to(ax.coords_to_point(x, y))
-            # self.add(dot)
-            # self.wait(0.1)
-            self.play(dot.animate.become(dot.copy().set_color(BLUE)), run_time=0.1)
-        # graph = ax.plot(lambda x: x ** 2, x_range=[0.001, 30, 0.5], use_smoothing=False)
-        # self.add(ax, graph)
-        # dot = Circle(radius=0.2, color=RED, fill_opacity=1).move_to(RIGHT*2)
+        graph = ax.plot(lambda x: x ** 2, x_range=[0.001, 30, 0.5], use_smoothing=False)
+        self.add(ax, graph)
+
+        # m = 100
+        # for n in range(int(0.01*m), int(30*m), int(0.2*m)):
+        #     x = n / 100
+        #     y = x ** 2
+        #     dot = Circle(radius=0.01, color=RED, fill_opacity=1).move_to(ax.coords_to_point(x, y))
+        #     self.play(dot.animate.become(dot.copy().set_color(BLUE)), run_time=0.1)
