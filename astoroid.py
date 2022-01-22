37e96385ec804fccf08795f273b7dca56c70515a
Mike Burr <mb@unintuitive.org>
2022-01-22 12:10:09 -0700

    .

--- astoroid.py
+++ astoroid.py
@@ -1,11 +1,11 @@
 from decimal import Decimal
 
-class ModularNumberLine:
-    modulus = Decimal(10)
+class ModularDecimal:
 
-    def __init__(self, x):
-        self.next = None
+    def __init__(self, x, modulus):
         self.x = x if isinstance(x, Decimal) else Decimal(x)
+        self.modulus = modulus
+        self.next = None
         self.r = self.x % self.modulus    # `r`emainder
         self.f = self.x - self.r          # `f`loor
         self.mm = self.f // self.modulus  # `m`odulus `m`ultiple
@@ -18,15 +18,20 @@ class ModularNumberLine:
             next_ = current.get_next()
             line.append(current)
             if next_ is not None and next_.mm != current.mm: # assumes positive monotonic
-                end = ModularNumberLine(current.f + current.modulus)
+                end = self.__class__(current.f + current.modulus)
                 end.mm = current.mm
                 end.f = current.f
                 line.append(end)
                 lines.append(line)
-                start = ModularNumberLine(next_.f)
+                start = self.__class__(next_.f)
                 line = [start]
             current = next_
         return lines # we can/should generator this mofo
+
+    def append(self, next_val):
+        next_ = self.__class__(next_val, self.modulus)
+        self.next = next_
+        return self.next
 
     def get_next(self):
         return self.next
@@ -39,52 +44,43 @@ class ModularNumberLine:
             self.mm
         )
 
-point = ModularNumberLine(0)
-root = point
+root = None
+previous = None
 m = 10
+modulus = 10
 for n in range(int(0*m), int(300*m), int(1.1*m)):
-    foo = ModularNumberLine(n/m)
-    point.next = foo
-    point = foo
-
-more_lines = root.iter_lines()
-    
+    current = ModularDecimal(n/m, modulus)
+    if previous is not None:
+        previous.next = current
+    previous = current
+    if root is None:
+        root = current
+        
 ap = []
-lines = []
-bar = root
-line = []
-
-while bar is not None:
-    ap.append(bar)
-    next_ = bar.get_next()
-    line.append(bar)
-    if next_ is not None and next_.mm != bar.mm: # assumes positive monotonic
-        end = ModularNumberLine(bar.f + bar.modulus)
-        end.mm = bar.mm
-        end.f = bar.f
-        line.append(end)
-        lines.append(line)
-        start = ModularNumberLine(next_.f)
-        line = [start]
-    bar = next_
+val = root
+while True:
+    ap.append(val)
+    val = val.get_next()
+    if val is None:
+        break
 
 def eq(a, b):
     diff = abs(float(a) - float(b))
     return diff < 0.0001
 
-x = ap[10]
+x = ap[9]
 assert eq(x.x, 9.9)
 assert eq(x.r, 9.9)
 assert eq(x.f, 0.0)
 assert eq(x.mm, 0.0)
 
-x = ap[11]
+x = ap[10]
 assert eq(x.x, 11.0)
 assert eq(x.r, 1.0)
 assert eq(x.f, 10.0)
 assert eq(x.mm, 1.0)
 
-x = ap[12]
+x = ap[11]
 assert eq(x.x, 12.1)
 assert eq(x.r, 2.1)
 assert eq(x.f, 10.0)
