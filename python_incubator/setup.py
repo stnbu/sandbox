fb328a402d7e63f959404dd661b9088b5210c293
Mike Burr <mb@unintuitive.org>
2022-02-01 11:26:35 -0700

    moved to https://github.com/stnbu/polynomier

--- python_incubator/setup.py
+++ python_incubator/setup.py
@@ -5,5 +5,5 @@ from setuptools import setup
 setup(
     name="python_incubator",
     version="0.0.4",
-    packages=["astoroid", "polynomier", "optigraph"],
+    packages=["astoroid", "optigraph"],
 )
